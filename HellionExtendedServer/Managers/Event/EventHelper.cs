998f3d06d644353a4e61136364db84d9214176c6
yungtechboy1 <yungtechboy1@gmail.com>
2017-03-11 01:38:19 -0600

    Welp Goodmorning...

--- HellionExtendedServer/Managers/Event/EventHelper.cs
+++ HellionExtendedServer/Managers/Event/EventHelper.cs
@@ -28,13 +28,50 @@ namespace HellionExtendedServer.Managers.Event
         public EventHelper()
         {
             //TODO Register Each Event here
-            //Spawn event
-            //BUG Causes Exception?!?!?!?
-            NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(PlayerSpawnRequest), new EventSystem.NetworkDataDelegate(HandelPlayerSpawnEvent));
+            //Active
+            NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(PlayerSpawnRequest), HandelPlayerSpawnEvent);
+            NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(CharacterDataResponse), HandelPlayerTransferEvent);
+
+            //TODO
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(EventSystem.InternalEventType.GetPlayer, new EventSystem.InternalEventsDelegate(LoginPlayer));//TODO maybe NEVER!
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(CharacterListResponse), new EventSystem.NetworkDataDelegate(this.RemoveDeletedCharacters));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(CheckDeletedMessage), new EventSystem.NetworkDataDelegate(this.CheckDeleted));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(PlayerSpawnRequest), new EventSystem.NetworkDataDelegate(this.PlayerSpawnRequestListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(PlayerRespawnRequest), new EventSystem.NetworkDataDelegate(this.PlayerRespawnRequestListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(SpawnObjectsRequest), new EventSystem.NetworkDataDelegate(this.SpawnObjectsRequestListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(SubscribeToObjectsRequest), new EventSystem.NetworkDataDelegate(this.SubscribeToSpaceObjectListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(UnsubscribeFromObjectsRequest), new EventSystem.NetworkDataDelegate(this.UnsubscribeFromSpaceObjectListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(TextChatMessage), new EventSystem.NetworkDataDelegate(this.TextChatMessageListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(TransferResourceMessage), new EventSystem.NetworkDataDelegate(this.TransferResourcesMessageListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(RefineResourceMessage), new EventSystem.NetworkDataDelegate(this.RefineResourceMessageListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(CheckInResponse), new EventSystem.NetworkDataDelegate(this.CheckInResponseListener));
+            //NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(PlayersOnServerRequest), new EventSystem.NetworkDataDelegate(this.PlayersOnServerRequestListener));
+            // NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(LatencyTestMessage), new EventSystem.NetworkDataDelegate(this.LatencyTestListener));
+            // NetworkController.Instance.NetContoller.EventSystem.AddListener(typeof(SaveGameMessage), new EventSystem.NetworkDataDelegate(this.SaveGameMessageListener));
 
 
         }
 
+        public void HandelPlayerTransferEvent(NetworkData data)
+        {
+            try
+            {
+                CharacterDataResponse CharacterDataResponse = data as CharacterDataResponse;
+
+
+                ResponseResult Response = CharacterDataResponse.Response;
+                string Message = CharacterDataResponse.Message;
+                string CharacterData = CharacterDataResponse.CharacterData;
+                string CharacterName = CharacterDataResponse.CharacterName;
+                string SteamId = CharacterDataResponse.SteamId;
+                long CharacterId = CharacterDataResponse.CharacterId;
+                ExecuteEvent(new HESPlayerTransferEvent(Response,Message,CharacterData,CharacterName,SteamId,CharacterId));
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.Error("Hellion Extended Server [SPAWN REQUEST EVENT ERROR] : " + ex.InnerException.ToString());
+            }
+        }
         public void HandelPlayerSpawnEvent(NetworkData data)
         {
             try
@@ -45,13 +82,40 @@ namespace HellionExtendedServer.Managers.Event
                 SpawnPointLocationType SpawnType = playerSpawnRequest.SpawnType;
                 long SpawPointParentID = playerSpawnRequest.SpawPointParentID;
                 GameScenes.SceneID ShipItemID = playerSpawnRequest.ShipItemID;
-                ExecuteEvent(new HESSpawnEvent(SpawnType,SpawPointParentID,ShipItemID));
+                ExecuteEvent(new HESSpawnEvent(SpawnType, SpawPointParentID, ShipItemID));
             }
             catch (Exception ex)
             {
                 Log.Instance.Error("Hellion Extended Server [SPAWN REQUEST EVENT ERROR] : " + ex.InnerException.ToString());
             }
         }
+        /*
+         * TODO LOADS OF WORK!
+        public void HandelPlayerLoginEvent(EventSystem.InternalEventData data)
+        {
+            if (!this.WorldInitialized)
+                this.InitializeWorld();
+            long index = (long)data.Objects[0];
+            if (this._players.ContainsKey(index))
+                this.NetworkController.ConnectPlayer(this._players[index], true);
+            else if (this.NetworkController.IsLocal)
+            {
+                CharacterDataResponse characterDataResponse = new CharacterDataResponse();
+                characterDataResponse.Response = ResponseResult.Success;
+                characterDataResponse.CharacterId = index;
+                string str1 = "Player_";
+                int localPlayerCounter = this.localPlayerCounter;
+                this.localPlayerCounter = localPlayerCounter + 1;
+                // ISSUE: variable of a boxed type
+                __Boxed<int> local = (ValueType)localPlayerCounter;
+                string str2 = str1 + (object)local;
+                characterDataResponse.CharacterName = str2;
+                this.PlayerTransfer((NetworkData)characterDataResponse);
+            }
+            else
+                this.NetworkController.GetPlayerData(index);
+        }
+        */
 
         public void RegisterEvent(EventListener e)
         {

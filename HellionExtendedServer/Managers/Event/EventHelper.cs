205b6ad0b4d5ecbbbef6df40a1717d2df4624438
yungtechboy1 <yungtechboy1@gmail.com>
2017-03-13 00:50:32 -0500

    Yea no way around  :(

--- HellionExtendedServer/Managers/Event/EventHelper.cs
+++ HellionExtendedServer/Managers/Event/EventHelper.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using HellionExtendedServer.Common;
 using HellionExtendedServer.Managers.Event.Player;
 using ZeroGravity;
 using ZeroGravity.Data;
+using ZeroGravity.Helpers;
 using ZeroGravity.Network;
 using NetworkController = HellionExtendedServer.Controllers.NetworkController;
 
@@ -34,11 +36,72 @@ namespace HellionExtendedServer.Managers.Event
         {
             //DELETE ALL Events
             ES2 = NetworkController.Instance.NetContoller.EventSystem;//Copies Events
+
+            //Unregister All Listeners
+            //Get All Listeners to Register
+            ThreadSafeDictionary<EventSystem.InternalEventType, EventSystem.InternalEventsDelegate> internalDataGroups = GetCurrentListenersInternal();
+            ThreadSafeDictionary<Type, EventSystem.NetworkDataDelegate> networkDataGroups = GetCurrentListenersNetwork();
+            if (internalDataGroups == null)
+            {
+                Log.Instance.Error("Error starting EventHandeler! Could not find all events!");
+                return;
+            }
+            List<Type> AddedTypes = new List<Type>();
+            foreach (KeyValuePair<Type, EventSystem.NetworkDataDelegate> entry in networkDataGroups)
+            {
+                NetworkController.Instance.NetContoller.EventSystem.RemoveListener(entry.Key,entry.Value);
+                if(AddedTypes.Contains(entry.Key))continue;
+                AddedTypes.Add(entry.Key);
+                NetworkController.Instance.NetContoller.EventSystem.AddListener(entry.Key, MassEventHandeler);
+            }
+
+            //TODO dead end!
+            //BUg actually! A BIG ONE!!!!!!!!
+
             Log.Instance.Debug("DELETED OLD LISTENER");
             NetworkController.Instance.NetContoller.EventSystem = null; //I listen First!
             Log.Instance.Debug("CHECK OLD LISTENER"+ES2.GetType().Namespace);
             NetworkController.Instance.NetContoller.EventSystem = new EventSystem2(this); //I listen First!
             Log.Instance.Debug("New One Created!");
+        }
+
+        public ThreadSafeDictionary<Type, EventSystem.NetworkDataDelegate>
+            GetCurrentListenersNetwork()
+        {
+            //HACK
+            try
+            {
+                BindingFlags bf = BindingFlags.Instance | BindingFlags.NonPublic;
+                FieldInfo mi = ES2.GetType().GetField("networkDataGroups", bf);
+                return (ThreadSafeDictionary<Type, EventSystem.NetworkDataDelegate>)mi.GetValue(mi);
+            }
+            catch (Exception Ex)
+            {
+                
+            }
+
+            return null;
+
+        }
+
+        public ThreadSafeDictionary<EventSystem.InternalEventType, EventSystem.InternalEventsDelegate>
+            GetCurrentListenersInternal()
+        {
+
+            //HACK
+            try
+            {
+                BindingFlags bf = BindingFlags.Instance | BindingFlags.NonPublic;
+                FieldInfo mi = ES2.GetType().GetField("internalDataGroups", bf);
+                return (ThreadSafeDictionary<EventSystem.InternalEventType, EventSystem.InternalEventsDelegate>)mi.GetValue(mi);
+            }
+            catch (Exception Ex)
+            {
+                
+            }
+
+            return null;
+
         }
 
         public void MassEventHandeler(NetworkData data)

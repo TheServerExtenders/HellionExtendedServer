e35140fa4ce96ac97e5db56750b722b1960ed26d
generalwrex <bjdroot@gmail.com>
2017-03-19 04:55:13 -0700

    More work and testing on the PluginBrowsers backend

--- HellionExtendedServer/Managers/Plugins/Browser/PluginBrowser.cs
+++ HellionExtendedServer/Managers/Plugins/Browser/PluginBrowser.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace HellionExtendedServer.Managers.Plugins.Website
 {
-    class PluginBrowser
+    internal static class PluginBrowser
     {
+        public static void DownloadPlugin(string fileName, string url)
+        {
+            try
+            {
+                Console.WriteLine("Trying to download the file..");
+
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "GET";
+                request.Proxy = null;
+                request.CookieContainer = new CookieContainer();
+                request.UserAgent = nameof(HellionExtendedServer);
+
+                long received = 0;
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    var buffer = new byte[1024];
+
+                    var fileStream = File.OpenWrite(fileName);
+                    using (Stream input = response.GetResponseStream())
+                    {
+                        var size = input.Read(buffer, 0, buffer.Length);
+                        while (size > 0)
+                        {
+                            fileStream.Write(buffer, 0, size);
+                            received += size;
+
+                            size = input.Read(buffer, 0, buffer.Length);
+                        }
+
+                        Console.WriteLine("File downloaded! : " + fileName + "| Size: " + received / 1024 + "Kb");
+                    }
+
+                    fileStream.Flush();
+                    fileStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }

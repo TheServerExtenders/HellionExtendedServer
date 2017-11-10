b3bc4a5140209a7563633dbb202e5e74a48bc5f2
Generalwrex <bjdroot@gmail.com>
2017-11-10 14:14:02 -0800

    * Moved the LogManager out of the Common dll

---
+++ HellionExtendedServer/Managers/LogManager.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace HellionExtendedServer
+{
+    public class Log
+    {
+        public static Logger Instance { get; private set; }
+
+        public Log()
+        {
+            Instance = LogManager.GetCurrentClassLogger();
+        }
+    }
+}
